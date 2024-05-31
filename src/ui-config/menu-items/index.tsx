72b987f71c7ea2c7328c08298706f03c14ff501e
GustavoGJesus <gustavogomesdev@proton.me>
2024-05-31 09:37:30 -0300

    feat: add redirect to v3

--- src/ui-config/menu-items/index.tsx
+++ src/ui-config/menu-items/index.tsx
@@ -65,6 +65,26 @@ const moreMenuItems: MoreMenuItem[] = [
     title: t`Developers`,
     icon: <BookOpenIcon />,
   },
+  {
+    link: 'https://docs.aave.com/portal/',
+    title: t`Go to V3`,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+        />
+      </svg>
+    ),
+  },
 ];
 
 const fiatEnabled = process.env.NEXT_PUBLIC_FIAT_ON_RAMP;
