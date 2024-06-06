e77adc328a5d5c85ba439bda609d89ee14dca3cf
GustavoGJesus <gustavogomesdev@proton.me>
2024-06-05 23:20:48 -0300

    fix: governance and stake build

--- src/modules/governance/utils/governanceProvider.tsx
+++ src/modules/governance/utils/governanceProvider.tsx
@@ -1,9 +1,12 @@
-import { AaveGovernanceService } from '@pollum-io/contract-helpers';
+import { PegasysGovernanceService } from '@pollum-io/contract-helpers';
 
 import { governanceConfig } from '../../../ui-config/governanceConfig';
 import { getProvider } from '../../../utils/marketsAndNetworksConfig';
 
-export const governanceContract = new AaveGovernanceService(getProvider(governanceConfig.chainId), {
-  GOVERNANCE_ADDRESS: governanceConfig.addresses.PEGASYS_GOVERNANCE_V2,
-  GOVERNANCE_HELPER_ADDRESS: governanceConfig.addresses.PEGASYS_GOVERNANCE_V2_HELPER,
-});
+export const governanceContract = new PegasysGovernanceService(
+  getProvider(governanceConfig.chainId),
+  {
+    GOVERNANCE_ADDRESS: governanceConfig.addresses.PEGASYS_GOVERNANCE_V2,
+    GOVERNANCE_HELPER_ADDRESS: governanceConfig.addresses.PEGASYS_GOVERNANCE_V2_HELPER,
+  }
+);
