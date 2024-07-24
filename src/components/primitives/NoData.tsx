5478618f4a49703e4a5d8354984e2ea5cdd1383d
GustavoGJesus <gustavogomesdev@proton.me>
2024-07-24 09:22:41 -0300

    chore: color no data symbol

--- src/components/primitives/NoData.tsx
+++ src/components/primitives/NoData.tsx
@@ -4,5 +4,9 @@ import React from 'react';
 export const NoData = <C extends React.ElementType>(
   props: TypographyProps<C, { component?: C }>
 ) => {
-  return <Typography {...props}>—</Typography>;
+  return (
+    <Typography {...props} sx={{ color: '#E0E0E0' }}>
+      —
+    </Typography>
+  );
 };
