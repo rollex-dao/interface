193dc055052ff3cb23832dd4ec466c586d81bd2b
GustavoGJesus <gustavogomesdev@proton.me>
2024-07-30 08:56:31 -0300

    feat: adjust list buttons size

--- src/modules/dashboard/lists/ListButtonsColumn.tsx
+++ src/modules/dashboard/lists/ListButtonsColumn.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@mui/material';
 import { ReactNode } from 'react';
-import { DASHBOARD_LIST_COLUMN_WIDTHS } from 'src/utils/dashboardSortUtils';
+// import { DASHBOARD_LIST_COLUMN_WIDTHS } from 'src/utils/dashboardSortUtils';
 
 interface ListButtonsColumnProps {
   children?: ReactNode;
@@ -13,9 +13,9 @@ export const ListButtonsColumn = ({ children, isColumnHeader = false }: ListButt
       sx={{
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'flex-end',
-        maxWidth: DASHBOARD_LIST_COLUMN_WIDTHS.BUTTONS,
-        minWidth: DASHBOARD_LIST_COLUMN_WIDTHS.BUTTONS,
+        justifyContent: 'center',
+        // maxWidth: DASHBOARD_LIST_COLUMN_WIDTHS.BUTTONS,
+        // minWidth: DASHBOARD_LIST_COLUMN_WIDTHS.BUTTONS,
         flex: isColumnHeader ? 1 : 1,
         '.MuiButton-root': {
           ml: '6px',
